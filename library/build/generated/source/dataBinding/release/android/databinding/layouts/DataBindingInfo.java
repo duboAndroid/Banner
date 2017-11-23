f6f027947ae464da0515f49515f014dac8890a1d
dubo_ <277627117@qq.com>
2017-11-23 19:09:00 +0800

    BGA refresh

---
+++ library/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="d800080b-2242-4668-a935-075622c95c1a", modulePackage="cn.bingoogolapple.refreshlayout", sdkRoot="C:\\My_other\\Android_SDK",layoutInfoDir="C:\\Users\\dubo\\Desktop\\Banner\\library\\build\\intermediates\\data-binding-info\\release",exportClassListTo="C:\\Users\\dubo\\Desktop\\Banner\\library\\build\\intermediates\\data-binding-info\\release\\_generated.txt",isLibrary=true,minSdk=11,enableDebugLogs=false,printEncodedError=true)
+public class DataBindingInfo {}
